3a816e3163f4cf63d03718efa911d21350503570
G.Koutourinis <koutougio@gmail.com>
2025-03-24 10:16:50 +0200

    Add project files.

---
+++ DataAccessLibrary/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public required string LastName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar()")]
+        public required string Afm { get; set; }
+    }
+}
---
+++ HotelPmsUI/Form1.cs
@@ -0,0 +1,10 @@
+namespace HotelPmsUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
