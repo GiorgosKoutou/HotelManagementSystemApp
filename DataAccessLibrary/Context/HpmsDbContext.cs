8883f569bb4ba567cf7a6e755f69186b4d308506
G.Koutourinis <koutougio@gmail.com>
2025-03-24 11:43:31 +0200

    Adding Migrations for database creatinon and customer column modification

---
+++ DataAccessLibrary/Context/HpmsDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Context
+{
+    public class HpmsDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Customer>()
+                        .HasIndex(c => c.Tin)
+                        .IsUnique();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionstring = ConfigurationManager.ConnectionStrings["HpmsMySql"].ConnectionString;
+
+                optionsBuilder.UseMySql(connectionstring, ServerVersion.AutoDetect(connectionstring));
+        }
+    }
+}
