66ffa3d143701246dd0329e524b51e06c290cf32
G.Koutourinis <koutougio@gmail.com>
2025-03-24 10:32:13 +0200

    Change the name of Form1 to MainForm

--- HotelPmsUI/Forms/MainForm.cs
+++ HotelPmsUI/Forms/MainForm.cs
@@ -1,8 +1,8 @@
 namespace HotelPmsUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
