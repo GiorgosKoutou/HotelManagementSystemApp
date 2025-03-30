061389bcf3971a49603d652a7ce2da6885d49688
G.Koutourinis <koutougio@gmail.com>
2025-03-30 12:01:50 +0300

    Implement Dependency Injection in Program.cs
    Added Dependency Injection using Microsoft.Extensions.DependencyInjection.
    Introduced a `ServiceProvider` property and updated the `Main` method
    to utilize a service collection for managing application services.
    Registered `HpmsDbContext` and customer-related services and forms
    in a new `ConfigurationServices` method.

--- HotelPmsUI/Program.cs
+++ HotelPmsUI/Program.cs
@@ -1,17 +1,50 @@
+using HotelPmsUI.ModelServices;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace HotelPmsUI
 {
     internal static class Program
     {
+
+        public static IServiceProvider? ServiceProvider { get; private set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            // Dependency Injection
+            var serviceCollection = new ServiceCollection();
+            ConfigurationServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            Application.Run(ServiceProvider.GetRequiredService<Forms.MainForm>());
+        }
+
+        private static void ConfigurationServices(ServiceCollection service)
+        {
+            /* Register of the DbContext with the service collection */
+            service.AddDbContext<DataAccessLibrary.Context.HpmsDbContext>();
+
+            /* Register of the CrudServices with the service collection */
+
+            // Customer Services
+            service.AddScoped<ModelServices.CustomerService>();
+            
+
+
+            /* Register of the forms with the service collection */
+
+            // Main Form
+            service.AddTransient<Forms.MainForm>();
+
+            // Customer Forms
+            service.AddTransient<Forms.Customer.CustomerCrudForm>();
+            service.AddTransient<Forms.Customer.CustomerListForm>();
         }
     }
 }
