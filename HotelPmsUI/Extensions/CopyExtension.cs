22e9b09a8ac090d4510ea6043b86544a195f7673
G.Koutourinis <koutougio@gmail.com>
2025-05-03 20:47:52 +0300

    Add CopyExtension class for deep object copying
    Introduces a new static class `CopyExtension` in the
    `HotelPMSCore.Extensions` namespace. This class features
    the `CopyData<T>` method, which enables deep copying of
    objects by serializing them to JSON and then
    deserializing them back to their original type.
    Necessary namespaces for JSON serialization are also
    imported.

---
+++ HotelPmsUI/Extensions/CopyExtension.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelPMSCore.Extensions
+{
+    public static class CopyExtension
+    {
+        public static T CopyData<T>(this T self)
+        {
+            var serialized = JsonConvert.SerializeObject(self);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
