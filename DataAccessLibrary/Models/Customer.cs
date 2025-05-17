a3dada148bc574dd4e4e139a457060f320df333d
G.Koutourinis <koutougio@gmail.com>
2025-05-17 13:30:32 +0300

    Add Display attributes and initialize string properties
    Updated `FirstName`, `LastName`, and `Tin` properties in `Customer.cs` to include `Display` attributes for better UI representation. Initialized these properties to `string.Empty` to prevent null values, enhancing data integrity and usability.

--- DataAccessLibrary/Models/Customer.cs
+++ DataAccessLibrary/Models/Customer.cs
@@ -14,15 +14,18 @@ namespace DataAccessLibrary.Models
 
         [Required]
         [Column(TypeName = "varchar(50)")]
-        public required string FirstName { get; set; }
+        [Display(Name = "Firts Name")]
+        public required string FirstName { get; set; } = string.Empty;
 
         [Required]
         [Column(TypeName = "varchar(50)")]
-        public required string LastName { get; set; }
+        [Display(Name = "Last Name")]
+        public required string LastName { get; set; } = string.Empty;
 
         [Required]
         [Column(TypeName = "varchar(50)")]
-        public required string Tin { get; set; }
+        [Display(Name = "Tax Identification Number")]
+        public required string Tin { get; set; } = string.Empty;
 
         [Column(TypeName = "varchar(50)")]
         public string Country { get; set; } = string.Empty;
