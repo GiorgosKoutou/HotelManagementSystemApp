5d7d10a69c3252cc5966611bd1accce8ebda23fa
G.Koutourinis <koutougio@gmail.com>
2025-06-04 20:50:24 +0300

    Add ColumnNames attribute class and update dependencies
    Introduced a new `ColumnNames` class in `ColumnNames.cs` with a nullable `Name` property, marked for use on properties. Added necessary `using` directives for relevant namespaces. Updated `DataAccessLibrary.csproj` to include a new package reference for `LinqKit.Microsoft.EntityFrameworkCore` version `8.1.8`, alongside existing Entity Framework Core references.

---
+++ DataAccessLibrary/AttributeMarkerClasses/ColumnNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.AttributeMarkerClasses
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColumnNames : Attribute
+    {
+        public string? Name { get; set; }
+    }
+}
